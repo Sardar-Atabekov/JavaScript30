80652389d65f653bcb4c229d624f5713c909f9e7
Sardar-Atabekov <43814348+Sardar-Atabekov@users.noreply.github.com>
2018-11-06 13:14:47 +0600

    Delete script.js

--- 03 - CSS Variables/script.js
+++
@@ -1,9 +0,0 @@
-var inputs = document.querySelectorAll("input");
-
-inputs.forEach(input => input.addEventListener("change", inputUpdat));
-inputs.forEach(input => input.addEventListener("mousemove", inputUpdat));
-
-function inputUpdat() {
-    var suffix = this.dataset.sizing || "";
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-}
